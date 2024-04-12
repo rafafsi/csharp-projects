14f53d8fff0bf6b2779fd3d19c7f83a0a66a4b05
Rafaela Inácio <rafaelafsinacio@gmail.com>
2024-04-12 11:55:46 +0100

    [RMQ] create sender, receiver 1 and 2

---
+++ RabbitReceiver2/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "RabbitMQ Receiver 2";
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+var exchange = "exchange-demo";
+var routingKey = "routing-key-demo";
+var queue = "queue-demo";
+
+channel.ExchangeDeclare(exchange, ExchangeType.Direct, true, false);
+channel.QueueDeclare(queue, false, false, false, null);
+channel.QueueBind(queue, exchange, routingKey);
+
+// channel quality of service 
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+	Task.Delay(TimeSpan.FromMilliseconds(2)).Wait();
+	var body = args.Body.ToArray();
+	string message = Encoding.UTF8.GetString(body);
+	Console.WriteLine($"Message received: {message}");
+	channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queue, false, consumer);
+Console.ReadLine();
+channel.BasicCancel(consumerTag);
+channel.Close();
+connection.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "RabbitMQ Sender";
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+var exchange = "exchange-demo";
+var routingKey = "routing-key-demo";
+var queue = "queue-demo";
+
+channel.ExchangeDeclare(exchange, ExchangeType.Direct, true, false);
+channel.QueueDeclare(queue, false, false, false, null);
+channel.QueueBind(queue, exchange, routingKey);
+
+
+for (int i = 0; i < 60; i++)
+	{
+	Console.WriteLine($"Sending message: {i}");
+	byte[] messageBody = Encoding.UTF8.GetBytes($"Message: #{i}");
+	channel.BasicPublish(exchange, routingKey, null, messageBody);
+	Thread.Sleep(1000);
+	}
+
+
+channel.Close();
+connection.Close();
